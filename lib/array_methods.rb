f3a67f01be01f253962fcef0a3c2f097fc8d0eac
wesley perez <wesley.m.perez@gmail.com>
2019-12-17 08:34:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+array.include?(element)
 end
 
 def using_sort(array)
-
+movies = ["why him?","The Grinch","Home Alone"]
+movies.sort
 end
 
 def using_reverse(array)
-
+movies = ["why him?","The Grinch","Home Alone"]
+movies.reverse
 end
 
 def using_first(array)
-
+movies = ["why him?","The Grinch","Home Alone"]
+movies.first
 end
 
 def using_last(array)
-
+movies = ["why him?","The Grinch","Home Alone"]
+movies.last
 end
 
 def using_size(array)
-
+movies = ["why him?","The Grinch","Home Alone"]
+movies.size
 end
